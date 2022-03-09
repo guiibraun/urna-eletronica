98e5513464821c9ed0c4d75eef48df429dd4b60c
Guilherme Alexandre Braun <guiabraun@hotmail.com>
2022-03-08 21:12:10 -0300

    Preenchendo Interface com Números

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let seuVotoPara = document.querySelector('.d-1-1 span').style.display = 'none'
+let seuVotoPara = document.querySelector('.d-1-1 span')
 
 let cargo = document.querySelector('.d-1-2 span')
 
@@ -9,3 +9,59 @@ let aviso = document.querySelector('.d-2')
 let lateral = document.querySelector('.d-1-right')
 
 let numeros = document.querySelector('.d-1-3')
+
+let etapaAtual = 0
+let numeroAtual = ''
+
+function comecarEtapa(){
+
+    let etapa = etapas[etapaAtual]
+
+    let numeroHtml = ''
+
+    for(let i=0;i<etapa.numeros;i++){
+        if(i === 0){
+            numeroHtml += '<div class="numero pisca"></div>'
+        }
+        numeroHtml += '<div class="numero"></div>'
+
+    }
+
+    seuVotoPara.style.display = 'none'
+    cargo.innerHTML = etapa.titulo
+    descricao.innerHTML = ''
+    aviso.style.display = 'none'
+    lateral.innerHTML = ''
+    numeros.innerHTML = numeroHtml
+}
+
+function atualizaInterface() {
+}
+
+function clicou(numeroTeclado) {
+    let numeroUrna = document.querySelector('.numero.pisca')
+    if(numeroUrna != null){
+        numeroUrna.innerHTML = numeroTeclado
+        numeroAtual = `${numeroAtual}${numeroTeclado}`
+        numeroUrna.classList.remove('pisca')
+        if(numeroUrna.nextElementSibling.classList !== null) {
+            numeroUrna.nextElementSibling.classList.add('pisca')
+        } else {
+            atualizaInterface()
+        }
+    }
+}
+
+function branco(){
+
+}
+
+function corrige(){
+
+}
+
+function confirma(){
+
+}
+
+comecarEtapa()
